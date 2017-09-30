1053c5978f6cb49d67410e239d07b8cea5b1d242
canyinghao <canyinghao@hotmail.com>
2017-09-30 15:26:55 +0800

    build

---
+++ app/src/main/java/com/canyinghao/canguidehint/demo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.canyinghao.canguidehint.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jianyang on 2017/9/30.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
